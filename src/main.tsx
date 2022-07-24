17e01fc7f3a4102a96c0577f491b9c469dbbc9d1
AlfredoCU <alfredo_11_cu@hotmail.com>
2022-07-23 23:39:17 -0500

    refactor: coments

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import { store } from "./store/store";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
+      {/* <App /> */}
       <AppHooks />
     </Provider>
   </React.StrictMode>
